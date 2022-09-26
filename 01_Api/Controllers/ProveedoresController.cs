50c14b6542504768696f01d3e480f5480f99dc1f
Nuria García <nuriag023@thecorner.cat>
2022-09-26 19:22:22 +0200

    Proveedores Ajax

---
+++ 01_Api/Controllers/ProveedoresController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using _04_Data.Data;
+
+namespace _01_Api.Controllers
+{
+    public class ProveedoresController : ApiController
+    {
+        private NorthWindTuneadoDbContext db = new NorthWindTuneadoDbContext();
+
+        // GET: api/Proveedores
+        public IList<Proveedor> GetEmpleado()
+        {
+            IList<Proveedor> proveedoresTabla = db.Proveedor.ToList();
+            IList<Proveedor> proveedores = new List<Proveedor>();
+            foreach (var proveedorTabla in proveedoresTabla)
+            {
+                Proveedor proveedor = new Proveedor();
+                proveedor.supplierID = proveedorTabla.supplierID;
+                proveedor.supplierName = proveedorTabla.supplierName;
+                proveedor.ContactName = proveedorTabla.ContactName;
+                proveedor.Address = proveedorTabla.Address;
+                proveedor.City = proveedorTabla.City;
+                proveedor.PostalCode = proveedorTabla.PostalCode;
+                proveedor.Country = proveedorTabla.Country;
+                proveedor.Phone = proveedorTabla.Phone;
+
+
+                proveedores.Add(proveedor);
+            }
+            return proveedores;
+        }
+
+        // GET: api/Proveedores/5
+        [ResponseType(typeof(Proveedor))]
+        public IHttpActionResult GetEmpleado(int id)
+        {
+            Proveedor proveedorTabla = db.Proveedor.Find(id);
+            if (proveedorTabla == null)
+            {
+                return NotFound();
+            }
+
+            Proveedor proveedor = new Proveedor();
+            proveedor.supplierID = proveedorTabla.supplierID;
+            proveedor.supplierName = proveedorTabla.supplierName;
+            proveedor.ContactName = proveedorTabla.ContactName;
+            proveedor.Address = proveedorTabla.Address;
+            proveedor.City = proveedorTabla.City;
+            proveedor.PostalCode = proveedorTabla.PostalCode;
+            proveedor.Country = proveedorTabla.Country;
+            proveedor.Phone = proveedorTabla.Phone;
+
+            return Ok(proveedor);
+        }
+
+        // PUT: api/Proveedores/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProveedor(int id, Proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != proveedor.supplierID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(proveedor).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProveedorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Proveedores
+        [ResponseType(typeof(Proveedor))]
+        public IHttpActionResult PostProveedor(Proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Proveedor.Add(proveedor);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = proveedor.supplierID }, proveedor);
+        }
+
+        // DELETE: api/Proveedores/5
+        [ResponseType(typeof(Proveedor))]
+        public IHttpActionResult DeleteProveedor(int id)
+        {
+            Proveedor proveedor = db.Proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return NotFound();
+            }
+
+            db.Proveedor.Remove(proveedor);
+            db.SaveChanges();
+
+            return Ok(proveedor);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProveedorExists(int id)
+        {
+            return db.Proveedor.Count(e => e.supplierID == id) > 0;
+        }
+    }
+}
--- 01_Api/Scripts/miAjax.js
+++ 01_Api/Scripts/miAjax.js
@@ -172,3 +172,137 @@ function categorias(param) {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+function proveedores(param) {
+
+	var miTabla = "";
+	var miTabla01 =
+		"   <table>" +
+		"        <thead>" +
+		"           <tr>" +
+		"               <th>supplierID</th>" +
+		"               <th>supplierName</th>" +
+		"               <th>ContactName</th>" +
+		"           </tr>" +
+		"        </thead>" +
+		"        <tbody>";
+	var miTabla02 = "";
+	var miTabla03 =
+		"       </tbody>" +
+		"   </table>";
+
+	var miTablaDetalle = "";
+	const opciones = {
+		method: 'GET'
+	}
+	//[{"EmployeeID":"1","LastName":"Davolio","FirstName":"Nancy","Title":"Sales Representative","TitleOfCourtesy":"Ms.","BirthDate":"1948-12-08 00:00:00","HireDate":"1992-05-01 00:00:00","Address":"507 - 20th Ave. E.Apt. 2A","City":"Seattle","Region":"WA","PostalCode":"98122","Country":"USA","HomePhone":"(206) 555-9857","Extension":"5467","Photo":null,"Notes":"Education includes a BA in psychology from Colorado State University in 1970. She also completed \"The Art of the Cold Call.\" Nancy is a member of Toastmasters International.","ReportsTo":"2","PhotoPath":"http:\/\/accweb\/emmployees\/davolio.bmp","Salary":"2954.55"},
+	if (param == null || param == 0) {
+		//fetch('https://tupelu.es/Ajax_y_Json/ajax_northwind_mysql/ajax_actual_fetch/detail/consulta.php', opciones)
+		//fetch('https://localhost:44351/Api/Proveedores',opciones)
+		fetch('https://localhost:44334/Api/Proveedores', opciones)
+			.then(respuesta => respuesta.json())
+			.then(resultado => {
+				resultado.forEach(elemento => {
+					miTabla02 = miTabla02 +
+						"                    <tr>" +
+						"                        <td>" + elemento.supplierID + "</td>" +
+						"                        <td>" + elemento.supplierName + "</td>" +
+						"                        <td>" + elemento.ContactName + "</td>" +
+						"                        <td><input type=\"button\" value=\"Detalle\" onclick=\"proveedores(" + elemento.supplierID + ")\"></td>" +
+						"                    </tr>";
+				});
+				miTabla = miTabla01 + miTabla02 + miTabla03;
+				document.getElementById("miTabla").innerHTML = miTabla;
+			});
+	} else {
+		//fetch('https://tupelu.es/Ajax_y_Json/ajax_northwind_mysql/ajax_actual_fetch/detail/consulta.php?id=' + param, opciones)
+		//fetch('https://localhost:44351/Api/Proveedores/' + param,opciones)
+		fetch('https://localhost:44334/Api/Proveedores/' + param, opciones)
+			.then(respuesta => respuesta.json())
+			.then(resultado => {
+				//	resultado.forEach(elemento => {            
+				miTablaDetalle = miTablaDetalle +
+					"   			   <table>" +
+					"                    <tr>" +
+					"						 <th>supplierID</th>" +
+					/*		"                        <td>" + elemento.EmployeeID + "</td>" +*/
+					//"                        <td>" + resultado[0].EmployeeID + "</td>" +
+					"                        <td>" + resultado.supplierID + "</td>" +
+					"                    </tr>" +
+					"                    <tr>" +
+					"						 <th>supplierName</th>" +
+					/*		"                        <td>" + elemento.FirstName + "</td>" +*/
+					//"                        <td>" + resultado[0].FirstName + "</td>" +
+					"                        <td>" + resultado.supplierName + "</td>" +
+					"                    </tr>" +
+					"                    <tr>" +
+					"						 <th>ContactName</th>" +
+					/*		"                        <td>" + elemento.LastName + "</td>" +*/
+					//"                        <td>" + resultado[0].LastName + "</td>" +
+					"                        <td>" + resultado.ContactName + "</td>" +
+					"                    </tr>" +
+
+					"                    <tr>" +
+					"						 <th>Address</th>" +
+					/*		"                        <td>" + elemento.LastName + "</td>" +*/
+					//"                        <td>" + resultado[0].LastName + "</td>" +
+					"                        <td>" + resultado.Address + "</td>" +
+					"                    </tr>" +
+
+					"                    <tr>" +
+					"						 <th>City</th>" +
+					/*		"                        <td>" + elemento.LastName + "</td>" +*/
+					//"                        <td>" + resultado[0].LastName + "</td>" +
+					"                        <td>" + resultado.City + "</td>" +
+					"                    </tr>" +
+
+					"                    <tr>" +
+					"						 <th>PostalCode</th>" +
+					/*		"                        <td>" + elemento.LastName + "</td>" +*/
+					//"                        <td>" + resultado[0].LastName + "</td>" +
+					"                        <td>" + resultado.PostalCode + "</td>" +
+					"                    </tr>" +
+
+					"                    <tr>" +
+					"						 <th>Country</th>" +
+					/*		"                        <td>" + elemento.LastName + "</td>" +*/
+					//"                        <td>" + resultado[0].LastName + "</td>" +
+					"                        <td>" + resultado.Country + "</td>" +
+					"                    </tr>" +
+
+
+					"                    <tr>" +
+					"						 <th>Phone</th>" +
+					/*		"                        <td>" + elemento.LastName + "</td>" +*/
+					//"                        <td>" + resultado[0].LastName + "</td>" +
+					"                        <td>" + resultado.Phone + "</td>" +
+					"                    </tr>" +
+
+
+					"   			   </table>";
+				//	});
+				//miTabla = miTabla01 + miTabla02 + miTabla03;    
+				document.getElementById("miTablaDetalle").innerHTML = miTablaDetalle;
+			});
+	}
+
+
+
+
+
+}
+
+
+
