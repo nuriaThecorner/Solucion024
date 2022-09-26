b7c080943b4f111d2ae62171e9d1e1a83d9ea8fd
Nuria García <nuriag023@thecorner.cat>
2022-09-26 17:36:29 +0200

    boton categoria

---
+++ 01_Api/Controllers/CategoriasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using _04_Data.Data;
+
+namespace _01_Api.Controllers
+{
+    public class CategoriasController : ApiController
+    {
+        private NorthWindTuneadoDbContext db = new NorthWindTuneadoDbContext();
+
+        // GET: api/Categorias
+        public IList<Categoria> GetEmpleado()
+        {
+            IList<Categoria> categoriasTabla = db.Categoria.ToList();
+            IList<Categoria> categorias = new List<Categoria>();
+            foreach (var categoriaTabla in categoriasTabla)
+            {
+                Categoria categoria = new Categoria();
+                categoria.CategoryID = categoriaTabla.CategoryID;
+                categoria.CategoryName = categoriaTabla.CategoryName;
+                categoria.Description = categoriaTabla.Description;
+
+                categorias.Add(categoria);
+            }
+            return categorias;
+        }
+
+        // GET: api/Categorias/5
+        [ResponseType(typeof(Categoria))]
+        public IHttpActionResult GetEmpleado(int id)
+        {
+            Categoria categoriaTabla = db.Categoria.Find(id);
+            if (categoriaTabla == null)
+            {
+                return NotFound();
+            }
+
+            Categoria categoria = new Categoria();
+            categoria.CategoryID = categoriaTabla.CategoryID;
+            categoria.CategoryName = categoriaTabla.CategoryName;
+            categoria.Description = categoriaTabla.Description;
+
+
+            return Ok(categoria);
+        }
+
+
+        // PUT: api/Categorias/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCategoria(int id, Categoria categoria)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != categoria.CategoryID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(categoria).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoriaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Categorias
+        [ResponseType(typeof(Categoria))]
+        public IHttpActionResult PostCategoria(Categoria categoria)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Categoria.Add(categoria);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = categoria.CategoryID }, categoria);
+        }
+
+        // DELETE: api/Categorias/5
+        [ResponseType(typeof(Categoria))]
+        public IHttpActionResult DeleteCategoria(int id)
+        {
+            Categoria categoria = db.Categoria.Find(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            db.Categoria.Remove(categoria);
+            db.SaveChanges();
+
+            return Ok(categoria);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CategoriaExists(int id)
+        {
+            return db.Categoria.Count(e => e.CategoryID == id) > 0;
+        }
+    }
+}
--- 01_Api/Scripts/miAjax.js
+++ 01_Api/Scripts/miAjax.js
@@ -78,4 +78,90 @@ function miAjax(param) {
 			document.getElementById("miTablaDetalle").innerHTML = miTablaDetalle;
 		});
 	}
+
+}
+
+
+
+
+
+function categorias(param) {
+
+	var miTabla = "";
+	var miTabla01 =
+		"   <table>" +
+		"        <thead>" +
+		"           <tr>" +
+		"               <th>CategoryID</th>" +
+		"               <th>CategoryName</th>" +
+		"               <th>Description</th>" +
+		"           </tr>" +
+		"        </thead>" +
+		"        <tbody>";
+	var miTabla02 = "";
+	var miTabla03 =
+		"       </tbody>" +
+		"   </table>";
+
+	var miTablaDetalle = "";
+	const opciones = {
+		method: 'GET'
+	}
+	//[{"EmployeeID":"1","LastName":"Davolio","FirstName":"Nancy","Title":"Sales Representative","TitleOfCourtesy":"Ms.","BirthDate":"1948-12-08 00:00:00","HireDate":"1992-05-01 00:00:00","Address":"507 - 20th Ave. E.Apt. 2A","City":"Seattle","Region":"WA","PostalCode":"98122","Country":"USA","HomePhone":"(206) 555-9857","Extension":"5467","Photo":null,"Notes":"Education includes a BA in psychology from Colorado State University in 1970. She also completed \"The Art of the Cold Call.\" Nancy is a member of Toastmasters International.","ReportsTo":"2","PhotoPath":"http:\/\/accweb\/emmployees\/davolio.bmp","Salary":"2954.55"},
+	if (param == null || param == 0) {
+		//fetch('https://tupelu.es/Ajax_y_Json/ajax_northwind_mysql/ajax_actual_fetch/detail/consulta.php', opciones)
+		//fetch('https://localhost:44351/Api/Categorias',opciones)
+		fetch('https://localhost:44334/Api/Categorias', opciones)
+			.then(respuesta => respuesta.json())
+			.then(resultado => {
+				resultado.forEach(elemento => {
+					miTabla02 = miTabla02 +
+						"                    <tr>" +
+						"                        <td>" + elemento.CategoryID + "</td>" +
+						"                        <td>" + elemento.CategoryName + "</td>" +
+						"                        <td>" + elemento.Description + "</td>" +
+						"                        <td><input type=\"button\" value=\"Detalle\" onclick=\"categorias(" + elemento.CategoryID + ")\"></td>" +
+						"                    </tr>";
+				});
+				miTabla = miTabla01 + miTabla02 + miTabla03;    
+				document.getElementById("miTabla").innerHTML = miTabla;
+			});
+	} else {
+		//fetch('https://tupelu.es/Ajax_y_Json/ajax_northwind_mysql/ajax_actual_fetch/detail/consulta.php?id=' + param, opciones)
+		//fetch('https://localhost:44351/Api/Categorias/' + param,opciones)
+		fetch('https://localhost:44334/Api/Categorias/' + param, opciones)
+			.then(respuesta => respuesta.json())
+			.then(resultado => {
+				//	resultado.forEach(elemento => {            
+				miTablaDetalle = miTablaDetalle +
+					"   			   <table>" +
+					"                    <tr>" +
+					"						 <th>CategoryID</th>" +
+					/*		"                        <td>" + elemento.EmployeeID + "</td>" +*/
+					//"                        <td>" + resultado[0].EmployeeID + "</td>" +
+					"                        <td>" + resultado.CategoryID + "</td>" +
+					"                    </tr>" +
+					"                    <tr>" +
+					"						 <th>CategoryName</th>" +
+					/*		"                        <td>" + elemento.FirstName + "</td>" +*/
+					//"                        <td>" + resultado[0].FirstName + "</td>" +
+					"                        <td>" + resultado.CategoryName + "</td>" +
+					"                    </tr>" +
+					"                    <tr>" +
+					"						 <th>Description</th>" +
+					/*		"                        <td>" + elemento.LastName + "</td>" +*/
+					//"                        <td>" + resultado[0].LastName + "</td>" +
+					"                        <td>" + resultado.Description + "</td>" +
+					"                    </tr>" +
+					"   			   </table>";
+				//	});
+			//miTabla = miTabla01 + miTabla02 + miTabla03;    
+				document.getElementById("miTablaDetalle").innerHTML = miTablaDetalle;
+			});
+	}
+
+
+
+
+
 }
