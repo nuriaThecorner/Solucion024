c67c22ef4f0e7e9ad3fc9d1a5881f05b259d0b2c
Nuria García <nuriag023@thecorner.cat>
2022-09-27 16:02:23 +0100

    Ajax Categoria Jquery

--- 01_Api/Scripts/miAjax.js
+++ 01_Api/Scripts/miAjax.js
@@ -453,3 +453,157 @@ function miAjaxJquery(param) {
 			});
 	}
 }
+
+
+
+
+
+
+
+
+
+
+function miAjaxJqueryCategorias(param) {
+	var miTablaJquery = "";
+	var miTablaJquery01 =
+		"   <table>" +
+		"        <thead>" +
+		"           <tr>" +
+		"               <th>CategoryID</th>" +
+		"               <th>CategoryName</th>" +
+		"               <th>Description</th>" +
+		"           </tr>" +
+		"        </thead>" +
+		"        <tbody>";
+	var miTablaJquery02 = "";
+	var miTablaJquery03 =
+		"       </tbody>" +
+		"   </table>";
+
+	var miTablaJqueryDetalle = "";
+	const opciones = {
+		method: 'GET'
+	}
+	/*Esta función monta un string con formato html. Con los datos de TODOS LOS REGISTROS DEL JSON
+	Y LO DEVUELVE*/
+	function manageDataTable(param) {
+		// una vez que obtenga el json parseo los resultados:
+		var miTablaJquery04 = "";
+
+		$.each(param, function (propName, propVal) {
+			miTablaJquery04 = miTablaJquery04 +
+				"                    <tr>" +
+				"                        <td>" + propVal.CategoryID + "</td>" +
+				"                        <td>" + propVal.CategoryName + "</td>" +
+				"                        <td>" + propVal.Description + "</td>" +
+				"                        <td><input type=\"button\" value=\"Detalle\" onclick=\"miAjaxJqueryCategorias(" + propVal.CategoryID + ")\"></td>" +
+				"                    </tr>";
+		});
+
+		return miTablaJquery04;
+	}
+
+	/*Esta función monta un string con formato html. Con los datos de EL ÚNICO REGISTRO DEL JSON
+	Y LO DEVUELVE*/
+	function manageDataTableDetail(param) {
+		// una vez que obtenga el json parseo los resultados:
+		var miTablaJquery05 = "";
+		miTablaJquery05 = miTablaJquery05 +
+			"   			   <table>" +
+			"                    <tr>" +
+			"						 <th>EmployeeID</th>" +
+			/*		"                        <td>" + elemento.EmployeeID + "</td>" +*/
+			/*		"                        <td>" + resultado[0].EmployeeID + "</td>" +*/
+			"                        <td>" + param.CategoryID + "</td>" +
+			"                    </tr>" +
+			"                    <tr>" +
+			"						 <th>FirstName</th>" +
+			/*		"                        <td>" + elemento.FirstName + "</td>" +*/
+			/*		"                        <td>" + param[0].FirstName + "</td>" +*/
+			"                        <td>" + param.CategoryName + "</td>" +
+			"                    </tr>" +
+			"                    <tr>" +
+			"						 <th>LastName</th>" +
+			/*		"                        <td>" + elemento.LastName + "</td>" +*/
+			/*		"                        <td>" + param[0].LastName + "</td>" +*/
+			"                        <td>" + param.Description + "</td>" +
+			"                    </tr>" +
+			"                    <tr>" +
+			"						 <th>Imagen</th>" +
+			/*		"                        <td><img src=\"https://tupelu.es/Ajax_y_Json/ajax_northwind_mysql/ajax_actual_fetch/detail/Images/categoria/" + elemento.EmployeeID + ".jpg\"></td>" +*/
+			/*		"                        <td><img src=\"https://tupelu.es/Ajax_y_Json/ajax_northwind_mysql/ajax_actual_fetch/detail/Images/categoria/" + param[0].EmployeeID + ".jpg\"></td>" +*/
+			"                        <td><img src=\"https://tupelu.es/Ajax_y_Json/ajax_northwind_mysql/ajax_actual_fetch/detail/Images/categoria/" + param.CategoryID + ".gif\"></td>" +
+			"                    </tr>" +
+			"   			   </table>";
+
+		return miTablaJquery05;
+	}
+	//[{"EmployeeID":"1","LastName":"Davolio","FirstName":"Nancy","Title":"Sales Representative","TitleOfCourtesy":"Ms.","BirthDate":"1948-12-08 00:00:00","HireDate":"1992-05-01 00:00:00","Address":"507 - 20th Ave. E.Apt. 2A","City":"Seattle","Region":"WA","PostalCode":"98122","Country":"USA","HomePhone":"(206) 555-9857","Extension":"5467","Photo":null,"Notes":"Education includes a BA in psychology from Colorado State University in 1970. She also completed \"The Art of the Cold Call.\" Nancy is a member of Toastmasters International.","ReportsTo":"2","PhotoPath":"http:\/\/accweb\/emmployees\/davolio.bmp","Salary":"2954.55"},
+	if (param == null || param == 0) {
+		//var apiUrl = 'https://tupelu.es/Ajax_y_Json/ajax_northwind_mysql/ajax_actual_fetch/detail/consulta.php';
+		//var apiUrl = 'https://localhost:44351/Api/Categorias'';
+		var apiUrl = 'https://localhost:44334/Api/Categorias';
+		//llamamos a un objeto de la librería jQuery y le pasamos la apiUrl
+		$.ajax({
+			//parámetros de configuración del objeto Ajax de jQuery
+			contentType: 'application/html; charset=utf-8',
+			type: 'GET',
+			dataType: 'html',
+			url: apiUrl,
+			async: true
+
+		})
+			//el objeto nos muestra lo que le ha devuelto la api como parámetro "res"
+			.done(function (res) {
+				//convertimos el parámetro "res" en un json
+				var objetoRes = JSON.parse(res);
+				//pasamos este json a una función manageDataTable() que nos montará la Tabla
+				var stringTable = manageDataTable(objetoRes);
+				//tomamos el string con la tabla montada y se lo pasamos a la div con id = miTablaJquery
+				//versión jQuery
+				$('#miTablaJquery').empty().append(stringTable);
+				////versión Javascript nativo
+				//document.getElementById("miTablaJquery").innerHTML = stringTable;
+
+			}).fail(function (res) {
+				console.log(res);
+
+			}).always(function (res) {
+				console.log(res);
+
+			});
+	} else {
+		//var apiUrl = 'https://tupelu.es/Ajax_y_Json/ajax_northwind_mysql/ajax_actual_fetch/detail/consulta.php/'+ param;
+		//var apiUrl = 'https://localhost:44351/Api/Categorias/'+ param;
+		var apiUrl = 'https://localhost:44334/Api/Categorias/' + param;
+		//llamamos a un objeto de la librería jQuery y le pasamos la apiUrl
+		$.ajax({
+			//parámetros de configuración del objeto Ajax de jQuery
+			contentType: 'application/html; charset=utf-8',
+			type: 'GET',
+			dataType: 'html',
+			url: apiUrl,
+			async: true
+
+		})
+			//el objeto nos muestra lo que le ha devuelto la api como parámetro "res"
+			.done(function (res) {
+				//convertimos el parámetro "res" en un json
+				var objetoRes = JSON.parse(res);
+				//pasamos este json a una función manageDataTable() que nos montará la Tabla
+				var stringTable = manageDataTableDetail(objetoRes);
+				//tomamos el string con la tabla montada y se lo pasamos a la div con id = miTablaJqueryCategorias
+				//versión jQuery
+				$('#miTablaJqueryDetalle').empty().append(stringTable);
+				////versión Javascript nativo
+				//document.getElementById("miTablaJqueryDetalle").innerHTML = stringTable;
+
+			}).fail(function (res) {
+				console.log(res);
+
+			}).always(function (res) {
+				console.log(res);
+
+			});
+	}
+}
